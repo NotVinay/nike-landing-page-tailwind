336d10625e6dc1fc44a114bfdce2aed11b3c4ee8
Vinay Patel <47640203+NotVinay@users.noreply.github.com>
2024-04-23 21:04:23 +0100

    Added nav section

---
+++ src/components/Nav.jsx
@@ -0,0 +1,41 @@
+import {headerLogo} from '../assets/images'
+import {hamburger} from '../assets/icons'
+import { navLinks } from '../constants'
+
+const Nav = () => {
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+        <nav className="flex justify-between items-center max-container">
+            <a href="/">
+                <img 
+                    src={headerLogo}
+                    alt="Logo"
+                    width={130}
+                    height={29}
+                />
+            </a>
+            <ul className='flex max-lg:hidden flex-1 justify-center items-center gap-16'>
+                {navLinks.map(link => (
+                    <li key={link.label}>
+                        <a
+                            href={link.href}
+                            className="font-montserrat leading-normal text-lg text-slate-gray">
+                                {link.label}
+                            </a>
+                    </li>
+                ))}
+            </ul>
+            <div className="lg:hidden">
+                <img
+                    src={hamburger}
+                    alt="menu"
+                    width={25}
+                    height={3}
+                 />
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Nav
